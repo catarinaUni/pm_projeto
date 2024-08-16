5832f1c03814f9216752440cb6b1f7b27c67995b
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-08-16 10:37:52 +0000

    update

--- routes.js
+++ routes.js
@@ -4,30 +4,30 @@ import * as userController from './controllers/usuarios.js'
 const routes = express.Router();
 
 routes.get("/usuarios", (req, res) => {
-    res.json(userController.listar())
+    res.status(200).json(userController.listar())
 })
 
 routes.get("/usuarios/:id", (req, res) => {
     const id = req.params.id;
-    res.json(userController.buscarPorId(id))
+    res.status(200).json(userController.buscarPorId(id))
 })
 
 routes.post("/usuarios", (req, res) => {
     const usuario = req.body;
     
-    res.json(userController.adicionarUsuario(usuario));
+    res.status(201).json(userController.adicionarUsuario(usuario));
 });
 
 routes.delete("/usuarios/:id", (req, res) => {
     const id = req.params.id;
-    res.json(userController.apagarUsuario(id));
+    res.status(200).json(userController.apagarUsuario(id));
 });
 
 routes.put("/usuarios/:id", (req, res) => {
     const id = req.params.id;
     const usuarioModificado = req.body;
     console.log(id, usuarioModificado)
-    res.json(userController.modificarUsuario(id, usuarioModificado))
+    res.status(200).json(userController.modificarUsuario(id, usuarioModificado))
 })
 
 
