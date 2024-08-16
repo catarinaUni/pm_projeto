bc1d67bb33a905d2a348fe788152be06c1cde38a
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-08-16 19:35:42 +0000

    upd

---
+++ banco/conexao.js
@@ -0,0 +1,40 @@
+import mysql from 'mysql2';
+
+
+class Conexao {
+    constructor() {
+        if (!Conexao.instance) {
+            this.conection = mysql.createConnection({
+                host: "localhost",
+                user: "root",
+                password: ""
+            })
+
+            this.conection.connect((err) => {
+                if (err) throw err;
+
+                console.log("Conectado ao banco")
+            })
+
+            Conexao.instance = this
+        }
+
+        return Conexao.instance;
+    }
+
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.conection.query((sql, args, err, results) => {
+                if (err) return reject(err)
+
+                resolve(results)
+            })
+        }
+        )
+    }
+}
+
+const instance = new Conexao()
+Object.freeze(instance)
+
+export default instance;
